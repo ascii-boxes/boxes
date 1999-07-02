66e1c2b429087a35c6e5b567eddb324fa1acb042
Thomas Jensen <tsjensen@users.noreply.github.com>
1999-07-02 11:47:11 +0000

    Initial revision

---
+++ src/lexer.h
@@ -0,0 +1,29 @@
+/*
+ *  File:             parser.h
+ *  Project Main:     boxes.c
+ *  Date created:     July 01, 1999 (Thursday, 13:43h)
+ *  Author:           Thomas Jensen
+ *                    tsjensen@stud.informatik.uni-erlangen.de
+ *  Version:          $Id$
+ *  Language:         ANSI C
+ *  Purpose:          Export symbols used by the parser files only
+ *  Remarks:          ---
+ *
+ *  Revision History:
+ *
+ *    $Log$
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+#ifndef PARSER_H
+#define PARSER_H
+
+
+void begin_speedmode();
+
+extern int speeding;
+
+
+#endif /*PARSER_H*/
+
+/*EOF*/                                                  /* vim: set sw=4: */
