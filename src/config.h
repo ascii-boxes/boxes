8c5e6199341410b5bbb393b06448d0d8f8e7f803
Thomas Jensen <tsjensen@users.noreply.github.com>
1999-07-22 12:26:10 +0000

    Initial revision

---
+++ src/config.h
@@ -0,0 +1,42 @@
+/*
+ *  File:             config.h
+ *  Project Main:     boxes.c
+ *  Date created:     July 4, 1999 (Sunday, 21:04h)
+ *  Author:           Copyright (C) 1999 Thomas Jensen
+ *                    tsjensen@stud.informatik.uni-erlangen.de
+ *  Version:          $Id$
+ *  Language:         ANSI C
+ *  Purpose:          Please compiler and ease porting
+ *
+ *  Remarks: o This program is free software; you can redistribute it and/or
+ *             modify it under the terms of the GNU General Public License as
+ *             published by the Free Software Foundation; either version 2 of
+ *             the License, or (at your option) any later version.
+ *           o This program is distributed in the hope that it will be useful,
+ *             but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *             GNU General Public License for more details.
+ *           o You should have received a copy of the GNU General Public
+ *             License along with this program; if not, write to the Free
+ *             Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *             MA 02111-1307  USA
+ *
+ *  Revision History:
+ *
+ *    $Log$
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define _POSIX_SOURCE
+#ifdef __sun__
+#define __EXTENSIONS__
+#endif
+
+
+#endif /*CONFIG_H*/
+
+/*EOF*/
