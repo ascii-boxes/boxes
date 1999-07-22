cc8471f243ba5ea8cc1a17a3c11c6a6084621fa9
Thomas Jensen <tsjensen@users.noreply.github.com>
1999-07-22 12:28:25 +0000

    Added GNU GPL disclaimer Added include config.h

--- src/shape.c
+++ src/shape.c
@@ -2,23 +2,40 @@
  *  File:             shape.c
  *  Project Main:     boxes.c
  *  Date created:     June 23, 1999 (Wednesday, 13:39h)
- *  Author:           Thomas Jensen
+ *  Author:           Copyright (C) 1999 Thomas Jensen
  *                    tsjensen@stud.informatik.uni-erlangen.de
- *  Version:          $Id: shape.c,v 1.1 1999/06/23 12:26:59 tsjensen Exp tsjensen $
+ *  Version:          $Id: shape.c,v 1.2 1999/06/25 18:52:28 tsjensen Exp tsjensen $
  *  Language:         ANSI C
  *  World Wide Web:   http://home.pages.de/~jensen/boxes/
  *  Purpose:          Shape handling and information functions
- *  Remarks:          ---
+ *
+ *  Remarks: o This program is free software; you can redistribute it and/or
+ *             modify it under the terms of the GNU General Public License as
+ *             published by the Free Software Foundation; either version 2 of
+ *             the License, or (at your option) any later version.
+ *           o This program is distributed in the hope that it will be useful,
+ *             but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *             GNU General Public License for more details.
+ *           o You should have received a copy of the GNU General Public
+ *             License along with this program; if not, write to the Free
+ *             Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *             MA 02111-1307  USA
  *
  *  Revision History:
  *
  *    $Log: shape.c,v $
+ *    Revision 1.2  1999/06/25 18:52:28  tsjensen
+ *    Added empty_side() function from boxes.c
+ *    Removed #include regexp.h
+ *
  *    Revision 1.1  1999/06/23 12:26:59  tsjensen
  *    Initial revision
  *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
+#include "config.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -26,7 +43,7 @@
 #include "boxes.h"
 
 static const char rcsid_shape_c[] =
-    "$Id: shape.c,v 1.1 1999/06/23 12:26:59 tsjensen Exp tsjensen $";
+    "$Id: shape.c,v 1.2 1999/06/25 18:52:28 tsjensen Exp tsjensen $";
 
 
 
