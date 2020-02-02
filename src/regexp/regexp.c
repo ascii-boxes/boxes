7ef0b3517ffb57bc536a000248c95cd76ff440ba
Thomas Jensen <boxes@thomasjensen.com>
2020-02-02 14:14:49 +0100

    Replace some misleading angle brackets with quotes #67

--- src/regexp/regexp.c
+++ src/regexp/regexp.c
@@ -25,7 +25,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <regexp.h>
+#include "regexp.h"
 #include "regmagic.h"
 
 
--- src/regexp/regsub.c
+++ src/regexp/regsub.c
@@ -25,7 +25,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <regexp.h>
+#include "regexp.h"
 #include "regmagic.h"
 
 
