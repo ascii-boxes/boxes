12fa4ea57e752782a00a60ab319df88588bb81f1
Thomas Jensen <boxes@thomasjensen.com>
2021-02-10 22:14:09 +0100

    Add test cases for colored unicode 'headline' design #1
    This design uses complex regular expressions, which must work on unicode characters.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 #
-# Low-tech test runner for boxes.
+# boxes - Command line filter to draw/remove ASCII boxes around text
+# Copyright (C) 1999  Thomas Jensen and the boxes contributors
 #
-# File:             testrunner.sh
-# Date created:     September 23, 2014 (Tuesday, 20:06h)
-# Author:           Thomas Jensen
-# _____________________________________________________________________
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+# License, version 2, as published by the Free Software Foundation.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# _____________________________________________________________________________________________________________________
+#
+# Low-tech test runner for boxes.
+# _____________________________________________________________________________________________________________________
 
 if [ $# -ne 1 ]; then
     echo 'Usage: testrunner.sh {-suite | <testCaseFile>}'
