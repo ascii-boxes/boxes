03c79973f0a8478637eb01f952eb42b9a8b5ecc7
Matthew Thompson <matthew.thompson@nasa.gov>
2023-11-26 12:37:04 -0500

    Remove ignore errors

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -159,16 +159,12 @@ function measure_coverage()
     fi
 }
 
-# NOTE: It is possible when the white box tests work that we can
-# remove the --ignore-errors unused option. But for now, we use it so
-# that the CI is "happy" and we can get a baseline.
 function consolidate_coverage()
 {
     echo -e "\nConsolidating test coverage ..."
     pushd ${OUT_DIR}/test-results || exit 1
     find . -name "*.info" | xargs printf -- '--add-tracefile %s\n' | xargs --exit \
         lcov --rc branch_coverage=1 --exclude '*/lex.yy.c' --exclude '*/parser.c' \
-             --ignore-errors unused \
              --output-file ../${COVERAGE_FILE} --add-tracefile ../${BASELINE_FILE}
     popd || exit 1
     echo ""
